1eb55033d115a17a555411a90227f3ac350547c5
Unknown <michal.m.zarach@gmail.com>
2017-11-16 14:47:56 +0100

    Remove cache

--- app/etc/env.php
+++ app/etc/env.php
@@ -59,39 +59,4 @@ return array (
   array (
     'date' => 'Sun, 22 Oct 2017 19:05:19 +0000',
   ),
-  'cache' => 
-  array (
-    'frontend' => 
-    array (
-      'default' => 
-      array (
-        'backend' => 'Cm_Cache_Backend_Redis',
-        'backend_options' => 
-        array (
-          'server' => 'cache',
-          'database' => '0',
-          'port' => '6379',
-        ),
-      ),
-      'page_cache' => 
-      array (
-        'backend' => 'Cm_Cache_Backend_Redis',
-        'backend_options' => 
-        array (
-          'server' => 'cache',
-          'port' => '6379',
-          'database' => '1',
-          'compress_data' => '0',
-        ),
-      ),
-      'http_cache_hosts' => 
-      array (
-        0 => 
-        array (
-          'host' => 'varnish',
-          'port' => '80',
-        ),
-      ),
-    ),
-  ),
 );
